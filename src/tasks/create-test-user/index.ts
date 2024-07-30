1b8430cc18adf354695cbdad9f336d529e5f1c15
baketti <e.bachetti1997@gmail.com>
2024-07-30 17:39:49 +0200

    Fixed create-user-test

--- src/tasks/create-test-user/index.ts
+++ src/tasks/create-test-user/index.ts
@@ -1,5 +1,5 @@
 import { User } from "@/models/server/User";
 import { UserRoles } from "@/models/common/UserCommon";
 export const createUserTest = async () => {
-  await User.create("ema@gmail.com", "12345678", UserRoles.Admin);
+  await User.create("admin@admin.it", "12345678", UserRoles.Admin);
 };
